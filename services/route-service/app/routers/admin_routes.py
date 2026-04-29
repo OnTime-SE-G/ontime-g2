aac1587c1d093e5cc9fb4f88e5691bc7e57fa885
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2026-04-30 01:17:48 +0530

    completed route service

--- services/route-service/app/routers/admin_routes.py
+++ services/route-service/app/routers/admin_routes.py
@@ -1,15 +1,15 @@
-#services/route-service/app/routers/admin_routes.py
-
-from fastapi import APIRouter, UploadFile, File, Depends, Form
+from fastapi import APIRouter, UploadFile, File, Depends, Form, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
+from app.models.db_route import RouteORM
 from app.services.kml_service import import_kml_file
 
 router = APIRouter(
     prefix="/api/v1/admin/routes",
     tags=["Admin Routes"]
 )
+
 
 @router.post("/add-route")
 def upload_kml(
@@ -18,3 +18,42 @@ def upload_kml(
     db: Session = Depends(get_db)
 ):
     return import_kml_file(file, route_name, db)
+
+
+@router.put("/{route_id}")
+def update_route(
+    route_id: int,
+    route_name: str = Form(...),
+    file: UploadFile = File(...),
+    db: Session = Depends(get_db)
+):
+    route = db.query(RouteORM).filter(RouteORM.id == route_id).first()
+
+    if not route:
+        raise HTTPException(status_code=404, detail="Route not found")
+
+    # remove old route first (stops cascade delete)
+    db.delete(route)
+    db.commit()
+
+    # recreate from uploaded KML
+    return import_kml_file(file, route_name, db)
+
+
+@router.delete("/{route_id}")
+def delete_route(
+    route_id: int,
+    db: Session = Depends(get_db)
+):
+    route = db.query(RouteORM).filter(RouteORM.id == route_id).first()
+
+    if not route:
+        raise HTTPException(status_code=404, detail="Route not found")
+
+    db.delete(route)
+    db.commit()
+
+    return {
+        "message": "Route deleted successfully",
+        "route_id": route_id
+    }
