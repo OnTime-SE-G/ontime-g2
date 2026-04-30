9433dba5d19f858f7cbc5d07c2774d228315ef45
Chamodh Nethsara <72559423+chamodhk@users.noreply.github.com>
2026-05-01 01:29:05 +0530

    Fixed a minor bug in docker postgres initialization

--- docker/init/01-databases.sql
+++ docker/init/01-databases.sql
@@ -1,3 +1,9 @@
 CREATE DATABASE route_db;
 CREATE DATABASE fleet_db;
 CREATE DATABASE ontime_test_db;
+
+\connect route_db;
+CREATE EXTENSION postgis;
+
+\connect ontime_test_db;
+CREATE EXTENSION postgis;
